7a3f4c37115b76f09bd28d2b64530609635c1721
xevolo <xevolo12@gmail.com>
2024-01-06 19:53:25 +0100

    v2

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿
+using Quizyy_wpf.Model;
 using Quizyy_wpf.View;
 using System.Reflection;
 using System.Text;
@@ -110,6 +111,69 @@ namespace Quizyy_wpf
             }
             
         }
+
+        private void UpdateDatabase()
+        {
+			using (var context = new MyBaseContext())
+			{
+				/*
+				string path = "E:\\Studia\\kck\\danefiszki2.txt";
+				string[] lines = File.ReadAllLines(path);
+				foreach (var line in lines)
+				{
+					string[] elements = line.Split(';');
+					if (elements.Length == 3)
+					{
+
+						int id = Convert.ToInt32(elements[0]);
+						string concept = elements[1];
+						string definition = elements[2];
+
+						var neww = new FlashCardsModel
+						{
+							id = id,
+							concept = concept,
+							definition = definition
+						};
+						context.FlashCards.Add(neww);
+					}
+				}
+				context.SaveChanges();
+				*/
+				/*
+				string path = "E:\\Studia\\kck\\danepytanie2.txt";
+				string[] lines = File.ReadAllLines(path);
+				foreach (var line in lines)
+				{
+					string[] elements = line.Split(';');
+					if (elements.Length == 6)
+					{
+
+						int id = Convert.ToInt32(elements[0]);
+						string question = elements[1];
+						string answer = elements[2];
+						string incorrectans1 = elements[3];
+						string incorrectans2 = elements[4];
+						string incorrectans3 = elements[5];
+
+
+						var neww = new WriteModel
+						{
+							id = id,
+							question = question,
+							answer = answer,
+							incorrectans1=incorrectans1,
+							incorrectans2=incorrectans2,
+							incorrectans3=incorrectans3
+						};
+						context.Writes.Add(neww);
+					}
+				}
+				context.SaveChanges();
+				*/
+				context.Dispose();
+			}
+		}
 
 
     }
