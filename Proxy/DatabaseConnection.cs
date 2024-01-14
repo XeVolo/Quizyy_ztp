87fd2b38490e4661ea6f3fb74fc7dae47cbd8e12
Mixus <mateuszokragly@gmail.com>
2024-01-14 22:50:59 +0100

    b

--- Proxy/DatabaseConnection.cs
+++ Proxy/DatabaseConnection.cs
@@ -7,17 +7,28 @@ using System.Threading.Tasks;
 
 namespace Quizyy_wpf.Proxy
 {
-	public interface DatabaseConnection
-	{
-		List<FlashCardsModel> GetFlashCardsList();
+    public class DatabaseConnection : IDatabaseConnection
+    {
 
-		List<WriteModel> GetWriteList();
+        public List<FlashCardsModel> GetFlashCardsList()
+        {
+            using var context = new MyBaseContext();
+            return context.FlashCards.ToList();
 
-<<<<<<< Updated upstream
-		void SaveFlashCards();
+        }
+        public List<WriteModel> GetWriteList()
+        {
+            using (var context = new MyBaseContext())
+            {
+                List<WriteModel> list = context.Writes.ToList();
+                return list;
+            }
+        }
+        public void SaveFlashCards()
+        {
+            /*using (var context = new MyBaseContext())
+            {
 
-		void SaveWriteList();
-=======
                 var isExist = context.FlashCards.FirstOrDefault(f => f.concept == flashCard.concept && f.definition == flashCard.definition);
                 if (isExist == null)
                 {
@@ -26,9 +37,23 @@ namespace Quizyy_wpf.Proxy
                     context.FlashCards.Add(flashCard);
                     context.SaveChanges();
                 }
-            } */
+            }*/
         }
->>>>>>> Stashed changes
 
-	}
+        public void SaveWriteList()
+        {
+            /*
+            using (var context = new MyBaseContext())
+            {
+                var isExist = context.Writes.FirstOrDefault(f => f.question == write.question);
+				if (isExist == null)
+				{
+					var lastId = context.Writes.OrderByDescending(f => f.id).Select(f => f.id).FirstOrDefault();
+					write.id = lastId + 1;
+					context.Writes.Add(write);
+					context.SaveChanges();
+				}
+            }*/
+        }
+    }
 }
