41e333533f111b15feda477967be1d0af94b6427
xevolo <xevolo12@gmail.com>
2024-01-14 21:57:42 +0100

    adasd

---
+++ Observer/PointsObserver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizyy_wpf.Observer
+{
+    public class PointsObserver : IObserver
+    {
+        public virtual void Update(int Points)
+        {
+
+        }
+        public virtual int GetPoints()
+        {
+            return 0;
+        }
+    }
+}
